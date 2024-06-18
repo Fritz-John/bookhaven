63dab1a44e912afe4876bc3c18c14f04e8c0aafc
Fritz John S. Saycon <61639958+Fritz-John@users.noreply.github.com>
2024-06-18 21:06:12 +0800

    Inputted migrations for Orders, Books, OrderDetails

---
+++ database/migrations/2024_06_18_110200_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->decimal('total_amount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2024_06_18_110211_create_order_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Books;
+use App\Models\Orders;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('unit_price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
